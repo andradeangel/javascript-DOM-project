f0a1e2015226f6794dad7ecd3ee450f8765c8342
Angel <andradevelop@gmail.com>
2026-01-08 16:26:52 -0400

    estilos del menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Hola Mundo")
+function menuSwitch() {
+    const switchMenu = document.getElementById("options");
+    const logoText = document.getElementById("logoText");
+    const navbar = document.getElementById("navbar");
+    const btn = document.getElementsByClassName("btn-101")[0];
+
+    switchMenu.classList.toggle("show");
+    logoText.classList.toggle("navbarOpen");
+    navbar.classList.toggle("navbarStyle");
+    btn.classList.toggle("btnStyle");
+
+
+    if (switchMenu.classList.contains("show")) {
+        document.body.style.overflow = "hidden";
+        // document.getElementsByClassName("btn-101")[0].style.backgroundColor = "var(--fifthColor)";
+        // document.getElementsByClassName("btn-101")[0].style.boxShadow = "3px 3px 5px rgba(0, 0, 0, 0.37), -3px -3px 5px rgba(0, 0, 0, 0.37)";
+        document.getElementById("icon").innerHTML = "close";
+    } else {
+        document.body.style.overflow = "auto";
+        document.getElementById("icon").innerHTML = "menu";
+        // document.getElementById("logo-text").style.color = "var(--textColor)";
+    }
+}
