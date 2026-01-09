6d0434eb94e61a4663214c0a7654fd056dbbeaf3
Angel <andradevelop@gmail.com>
2026-01-09 14:49:25 -0400

    arreglando el comportamiento

--- js/main.js
+++ js/main.js
@@ -11,13 +11,8 @@ function menuSwitch() {
 
 
     if (switchMenu.classList.contains("show")) {
-        document.body.style.overflow = "hidden";
-        // document.getElementsByClassName("btn-101")[0].style.backgroundColor = "var(--fifthColor)";
-        // document.getElementsByClassName("btn-101")[0].style.boxShadow = "3px 3px 5px rgba(0, 0, 0, 0.37), -3px -3px 5px rgba(0, 0, 0, 0.37)";
         document.getElementById("icon").innerHTML = "close";
     } else {
-        document.body.style.overflow = "auto";
         document.getElementById("icon").innerHTML = "menu";
-        // document.getElementById("logo-text").style.color = "var(--textColor)";
     }
 }
